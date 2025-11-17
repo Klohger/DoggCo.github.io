093284a987c1e81aebdf85c8e9e971b8b7cc0483
KLohger <fanimdude32@gmail.com>
2025-11-17 23:36:22 +0100

    ouch

--- hamburger.js
+++ hamburger.js
@@ -1,19 +1,24 @@
+/**
+ * @name get_hamburg
+ * @function
+ * @returns {[HTMLElement, HTMLMedia]}
+ */
 
 document.addEventListener('mousedown', (ev) => { /** @type {HTMLImageElement} */
-    /**@type {[HTMLElement, HTMLAudioElement]}*/const [image, audio] = get_hamburg();
+    /**@type {[HTMLElement, HTMLMediaElement]}*/const [hamburger, audio] = get_hamburg();
     {
         const m = Math.min(window.innerWidth, window.innerHeight) * 0.05;
-        image.style.top = `${(ev.clientY + window.scrollY) - m}px`
-        image.style.left = `${(ev.clientX + window.scrollX) - m}px`
+        hamburger.style.top = `${(ev.clientY + window.scrollY) - m}px`
+        hamburger.style.left = `${(ev.clientX + window.scrollX) - m}px`
     };
     audio.preservesPitch = false;
     
     audio.playbackRate = 1 + (Math.random()*0.125-0.0625);
     audio.volume = 0.25;
     audio.addEventListener('play', () => {
-        HAMBURGERS.appendChild(image)
-        void image.offsetHeight;
-        image.style.opacity = 1;
+        HAMBURGERS.appendChild(hamburger)
+        void hamburger.offsetHeight;
+        hamburger.style.opacity = 1;
     });
     audio.play();
 });
